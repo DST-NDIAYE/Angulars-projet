701fbae34fa81407e4022bc071e27cd46b16db9e
SERIGNE BASSIROU DIAMOU SERIGNE TOUBA NDIAYE <dstndiaye@gmail.com>
2022-01-29 21:50:35 +0000

    Initialisation de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Livrei';
+
+
+  constructor(){
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDd4-qPhUY-8_IOlp1Hb3FoA18i8_9kClc",
+      authDomain: "livre-a5616.firebaseapp.com",
+      projectId: "livre-a5616",
+      storageBucket: "livre-a5616.appspot.com",
+      messagingSenderId: "1063723626872",
+      appId: "1:1063723626872:web:c37f811524e8da10be988d",
+      measurementId: "G-5Z7P2Z171N"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
+
 }
